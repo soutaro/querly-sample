3c097fba995f775c2d346f35b0d0348a2be5e23d
Soutaro Matsumoto <matsumoto@soutaro.com>
2018-04-25 00:15:43 +0900

    aaaaaaa

--- a.rb
+++ a.rb
@@ -4,3 +4,19 @@ end
 raise Error
 
 puts "Hello world"
+
+
+def import_issues!(build, issues)
+  timeout = (ENV["PULL_REQUEST_BUILD_RESULT_IMPORT_TIMEOUT"].presence || 10).to_i.minutes
+  expires_at = Time.current + timeout
+
+  issues.each do |issue|
+    raise TimeoutError if expires_at > Time.current
+  end
+
+rescue
+  build.repository.organization.with_octokit do |octokit|
+    commit_status!(octokit, build, octokit)
+  end
+end
+
